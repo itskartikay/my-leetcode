dc95cd8713cf045e8a2b339e4aaad63121cb563c
Kartikay Sharma <95382213+itskartikay@users.noreply.github.com>
2023-05-22 00:18:53 +0530

    Create Generate Fibonacci Sequence.js

---
+++ JavaScript/Generate Fibonacci Sequence.js
@@ -0,0 +1,20 @@
+/**
+ * @return {Generator<number>}
+ */
+var fibGenerator = function*() {
+    let a=0, b=1;
+    yield a
+    yield b
+    while(true){
+        [a,b]=[b,a+b];
+        yield b;
+    }
+
+    
+};
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
